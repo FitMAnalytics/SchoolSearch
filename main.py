08a3edf20032246a3ad53c9a4973daaf8727da55
FitMAnalytics <zygong1994@gmail.com>
2024-12-19 16:20:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 # Replace with your U.S. Census Bureau API key
-API_KEY = "YOUR_API_KEY"
+API_KEY = "ea4cf4edca163a7682936c1f208a8cee356d74f1"
 
 # Define API endpoint and parameters
 BASE_URL = "https://api.census.gov/data/2021/acs/acs5"
